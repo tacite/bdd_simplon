f12329505dcb197ca59c6a00c5d4e630326b9fde
hesolean <lndubourg@gmail.com>
2024-07-18 14:57:27 +0200

    code commenté et vérifié

--- azure_resources/1_azure_resources.sh
+++ azure_resources/1_azure_resources.sh
@@ -11,77 +11,56 @@ SKU_SERVER=Standard_B1ms
 ADMIN_PASSWORD=SadaHe111
 ADMIN_USER=adminsadahe
 DATABASE_NAME=sadaheformations
-# Storage
-# STORAGE_NAME=formationsadahestorage2
+# Function
 SKUNAME=Standard_LRS
-CONTAINER_NAME=sadaheformationscontainer
 UNIVERSAL_STORAGE_NAME=sadahestorage
 IMAGE_NAME="helenedubourg/sadahescrapy"
 ENVIRONMENT_NAME=sadaheenvironment
-# SUBSCRIPTION_ID="029b3537-0f24-400b-b624-6058a145efe1"
 APP_FUNCTION_NAME=sadahescrapyfunction
+FUNCTION_NAME=scrapytimer
 
 # Erase .env if exist to renew values
 if [ -f ".env" ]; then
     rm ".env"
 fi
 
-# Create resources group
-az group create \
-    --name $RESOURCE_GROUP \
-    --location $LOCATION
+# Create resources group is does not exist
+if ! az group show --name $RESOURCE_GROUP &>/dev/null; then
+  az group create --name $RESOURCE_GROUP --location $LOCATION
+fi
 
 echo "___RESSOURCES_GROUP___ finish"
 
-# # ___STORAGE___
-
-# # Create storage account
-# az storage account create \
-#     --name $STORAGE_NAME \
-#     --resource-group $RESOURCE_GROUP \
-#     --location $LOCATION \
-#     --sku $SKUNAME
-
-# # Get storage key
-# STORAGE_KEY=$(az storage account keys list \
-#     --resource-group $RESOURCE_GROUP \
-#     --account-name $STORAGE_NAME \
-#     --query '[0].value' \
-#     --output tsv)
-
-# # Create storage container
-# az storage container create \
-#     --name $CONTAINER_NAME \
-#     --account-name $STORAGE_NAME \
-#     --account-key $STORAGE_KEY
-
-# echo "___STORAGE___ finish"
-
 # ___DATABASE___
 
-# Create flexible server "Development"
-# az postgres flexible-server create \
-#     --name $SERVER_NAME \
-#     --resource-group $RESOURCE_GROUP \
-#     --location $LOCATION \
-#     --admin-password $ADMIN_PASSWORD \
-#     --admin-user $ADMIN_USER \
-#     --sku-name $SKU_SERVER \
-#     --tier Burstable \
-#     --version 12 \
-#     --database-name $DATABASE_NAME\
+# Create flexible server "Development" if does not exist
+if ! az postgres flexible-server show --name $SERVER_NAME --resource-group $RESOURCE_GROUP &>/dev/null; then
+  az postgres flexible-server create \
+      --name $SERVER_NAME \
+      --resource-group $RESOURCE_GROUP \
+      --location $LOCATION \
+      --admin-password $ADMIN_PASSWORD \
+      --admin-user $ADMIN_USER \
+      --sku-name $SKU_SERVER \
+      --tier Burstable \
+      --version 12 \
+      --database-name $DATABASE_NAME
+fi
 
-# Configure connexion parameters
-# az postgres flexible-server firewall-rule create \
-#     --resource-group $RESOURCE_GROUP \
-#     --name $SERVER_NAME \
-#     --rule-name AllowAll \
-#     --start-ip-address 0.0.0.0 \
-#     --end-ip-address 0.0.0.0
+# Configure connexion parameters if does not exist
+if ! az postgres flexible-server firewall-rule show --resource-group $RESOURCE_GROUP --name $SERVER_NAME --rule-name AllowAll &>/dev/null; then
+  az postgres flexible-server firewall-rule create \
+      --resource-group $RESOURCE_GROUP \
+      --name $SERVER_NAME \
+      --rule-name AllowAll \
+      --start-ip-address 0.0.0.0 \
+      --end-ip-address 0.0.0.0
+fi
 
 # Wait for the PostgreSQL server to be available and retrieve "SERVER_URL="
+SERVER_URL=""
 while [ -z "$SERVER_URL" ]; do
-    # Check server status
+    # Vérifier l'état du serveur
     SERVER_STATE=$(az postgres flexible-server show \
         --name $SERVER_NAME \
         --resource-group $RESOURCE_GROUP \
@@ -104,46 +83,43 @@ done
 
 echo "PostgreSQL server is ready with URL: $SERVER_URL"
 
-# Create PostgreSQL database
-# az postgres flexible-server db create \
-#     --resource-group $RESOURCE_GROUP \
-#     --server-name $SERVER_NAME \
-#     --database-name $DATABASE_NAME
+# Create PostgreSQL database if does not exist
+if ! az postgres flexible-server db show --resource-group $RESOURCE_GROUP --server-name $SERVER_NAME --database-name $DATABASE_NAME &>/dev/null; then
+  az postgres flexible-server db create \
+      --resource-group $RESOURCE_GROUP \
+      --server-name $SERVER_NAME \
+      --database-name $DATABASE_NAME
+fi
 
 echo "___DATABASE___ finish"
 
 # ___FUNCTION___
 
-# mettre à niveau l'extension Azure Container Apps
+# Upgrade the Azure Container Apps extension
 az extension add --name containerapp --upgrade -y
 az provider register --namespace Microsoft.Web 
 az provider register --namespace Microsoft.App 
 az provider register --namespace Microsoft.OperationalInsights
 
-# Créer un groupe de ressources
-if ! az group show --name $RESOURCE_GROUP &>/dev/null; then
-  az group create --name $RESOURCE_GROUP --location $LOCATION
+# Create Azure Container App environment
+az containerapp env create --name $ENVIRONMENT_NAME \
+  --enable-workload-profiles \
+  --resource-group $RESOURCE_GROUP \
+  --location $LOCATION
+
+# Create universal storage group if does not exist
+if ! az storage account show --name $UNIVERSAL_STORAGE_NAME &>/dev/null; then
+  az storage account create --name $UNIVERSAL_STORAGE_NAME \
+    --location $LOCATION \
+    --resource-group $RESOURCE_GROUP \
+    --sku $SKUNAME
 fi
 
-# Créer un environnement Azure Container App
-# az containerapp env create --name $ENVIRONMENT_NAME \
-#   --enable-workload-profiles \
-#   --resource-group $RESOURCE_GROUP \
-#   --location $LOCATION
-
-# Créer un groupe de stockage universel
-# if ! az storage account show --name $UNIVERSAL_STORAGE_NAME &>/dev/null; then
-#   az storage account create --name $UNIVERSAL_STORAGE_NAME \
-#     --location $LOCATION \
-#     --resource-group $RESOURCE_GROUP \
-#     --sku Standard_LRS
-# fi
-
-# Vérifier que l'environnement est prêt
+# Check if environment is ready
 az containerapp env show -n $ENVIRONMENT_NAME \
   -g $RESOURCE_GROUP
 
-# Créer une application de fonction
+# Create function app
 az functionapp create --name $APP_FUNCTION_NAME \
   --storage-account $UNIVERSAL_STORAGE_NAME \
   --environment $ENVIRONMENT_NAME \
@@ -153,16 +129,16 @@ az functionapp create --name $APP_FUNCTION_NAME \
   --runtime dotnet-isolated \
   --image $IMAGE_NAME
 
-# ajouter les variables d'environnement pour la connexion à la bdd pour scrapy
+# Add env variables for scrapy
 az functionapp config appsettings set --name $APP_FUNCTION_NAME \
   --resource-group $RESOURCE_GROUP \
   --settings "PGUSER=$ADMIN_USER" "PGPASSWORD=$ADMIN_PASSWORD" "PGHOST='$SERVER_URL'" "PGPORT=5432" "PGDATABASE=$DATABASE_NAME"
 
 
-# Vérifier la fonction
+# Check function
 az functionapp function show --resource-group $RESOURCE_GROUP \
   --name $APP_FUNCTION_NAME \
-  --function-name scrapytimer \
+  --function-name $FUNCTION_NAME \
   --query invokeUrlTemplate
 
 # Save variables in .env file
@@ -179,11 +155,12 @@ PGPASSWORD=$ADMIN_PASSWORD
 DATABASE_URL="postgresql+psycopg2://$ADMIN_USER:$ADMIN_PASSWORD@$SERVER_URL:5432/$DATABASE_NAME"
 SKU_SERVER=$SKU_SERVER
 SERVER_NAME=$SERVER_NAME
-# ___STORAGE___
-STORAGE_NAME=$STORAGE_NAME
-SKUNAME=$SKUNAME
-#STORAGE_KEY="$STORAGE_KEY"
-CONTAINER_NAME=$CONTAINER_NAME
+# ___FUNCTION___
+UNIVERSAL_STORAGE_NAME=$UNIVERSAL_STORAGE_NAME
+IMAGE_NAME=$IMAGE_NAME
+ENVIRONMENT_NAME=$ENVIRONMENT_NAME
+APP_FUNCTION_NAME=$APP_FUNCTION_NAME
+FUNCTION_NAME=$FUNCTION_NAME
 EOT
 
 echo ".env file created successfully with the following content:"
